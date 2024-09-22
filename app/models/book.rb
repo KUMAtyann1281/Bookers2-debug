30f55c7a66f2c0276bafee838bf07fb5d115ef37
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2024-09-22 07:37:01 +0000

    [Finish] book一覧でいいね多い順に非同期通信で再表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
 
   validates :title,
@@ -13,8 +14,8 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
-  
+
+
 
   def self.search_for(content, method)
     if method == 'perfect'
